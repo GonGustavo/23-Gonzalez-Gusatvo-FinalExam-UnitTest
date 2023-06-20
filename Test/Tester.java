6e08115edf658bb6ac81a054c45b079b326fc6af
Gustavo Gonzalez <gustavo.gonzalez@bellevuecollege.edu>
2023-06-20 11:14:00 -0700

    directory issues

--- Test/Tester.java
+++ Test/Tester.java
@@ -1,10 +1,12 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class Tester {
 
     @Test
     void negativeNumber(){
-
+    assertThrows()
 
     }
     @Test
